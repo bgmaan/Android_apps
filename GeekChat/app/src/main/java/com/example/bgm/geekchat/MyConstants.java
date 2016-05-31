fd36b8bd0311cb1fac62ccd23845ebd9a453e3a2
bgmaan <vzirook@gmail.com>
2016-05-31 10:44:04 +0200

    First commit

--- GeekChat/app/src/main/java/com/example/bgm/geekchat/MyConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.bgm.geekchat;
-
-/**
- * Created by bgm on 18.05.16.
- */
-public class MyConstants {
-    public static final String FIREBASE_URL = "https://glowing-fire-8710.firebaseio.com/";
-    public static String idUser ="";
-}
